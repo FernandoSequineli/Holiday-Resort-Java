ca388f543497a638b4122485a60e9060aad0ba12
Fernando Sequineli <56481201+FernandoSequineli@users.noreply.github.com>
2021-02-27 10:08:39 +0000

    Update HolidayResort.java

--- HolidayResort.java
+++ HolidayResort.java
@@ -2,16 +2,7 @@
 import java.util.Random;
 import java.util.Scanner;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- * 
- * @author fernando
- */
 
 public class HolidayResort {
     private String clientName;
